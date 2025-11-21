cf0dd2cee8d2bbafbec6ac971b224779c411e64c
Joao Silva <joaosilvaa.work@gmail.com>
2025-11-21 21:33:03 +0000

    add exercises

---
+++ cal_stack.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+numInput = input()
+
+num = list(map(int,numInput.split()))
+
+stack = deque(num)
+
+print(f"deque({list(stack)})")
+
+while stack:
+    print(stack.pop() * 2)
+
---
+++ queue_process.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+wordsInput = input()
+
+words = wordsInput.split()
+
+queue = deque(words)
+
+print(f"deque({list(queue)[::-1]})")
+
+for word in queue:
+    if 'a'in word:
+        print(word)
+
+
